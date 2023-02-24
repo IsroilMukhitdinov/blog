0f92aac02d4d034b33efcb6e803b7eeb8c22a77b
IsroilMukhitdinov <isroillisper@gmail.com>
2023-02-24 10:59:49 +0500

    spin image with music

--- static/js/main.js
+++ static/js/main.js
@@ -2,21 +2,38 @@ var music = new Audio("../music/katrinalil.mp3");
 
 var checkbox = document.querySelector("input[name=checkbox]");
 
+var image = document.getElementById("profile-picture");
+
+function goSnipping() {
+  image.classList.add("spin");
+}
+
+function stopSpinning() {
+  image.classList.remove("spin");
+}
+
+function playMusic() {
+  music.play();
+}
+
+function stopMusic() {
+  music.pause();
+  music.currentTime = 0;
+}
+
 checkbox.addEventListener('change', function() {
   if (this.checked) {
     console.log("Checkbox is checked...");
+    goSnipping();
     playMusic();
   } else {
     console.log("Checkbox is not checked...");
+    stopSpinning();
     stopMusic();
   }
 });
 
-function playMusic() {
-    music.play();
-}
 
-function stopMusic() {
-    music.pause();
-    music.currentTime = 0;
-}
+
+
+
